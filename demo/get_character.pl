b76f34399bb808ab8676e09c6f1a8d3b35e01866
Leam Hall <leamhall@gmail.com>
2020-11-07 10:23:02 -0500

    Demo of get_character.pl.

--- demo/get_character.pl
+++ demo/get_character.pl
@@ -10,24 +10,88 @@ use strict;
 use warnings;
 
 use DBI;
+use File::Basename;
+use Getopt::Long;
+
+my $sth;
+my $return;
 my $dbfile  = 'tmp/data/cadets.db';
+my $dbf;
 my $dbh;
+my $column;
+my $like;
+my $table;
+my $query = "SELECT * from ";
+
+GetOptions(
+  "c=s"   => \$column,
+  "d=s"   => \$dbf,
+  "l=s"   => \$like,
+  "t=s"   => \$table,
+);
+
+
+if ( $dbf ) {
+  $dbfile = $dbf;
+} else {
+  $dbfile = 'tmp/data/cadets.db';
+}
+
 if ( -f $dbfile ) {
   $dbh     = DBI->connect("dbi:SQLite:dbname=$dbfile", "", "");
 } else {
   die "Can't find $dbfile: $!";
 }
 
-my $sth     = $dbh->prepare("SELECT * FROM cadets WHERE last_name LIKE 'Domici';");
-my $return  = $sth->execute() or die "$DBI::errstr";
+if ( $table ) {
+  $query  .= "$table ";
+} else {
+  # Assumes the table requested is the same as the filename.
+  # Likely a poor assumption.
+  my ($filename)  = fileparse($dbfile);
+  ( $table = $filename ) =~ s/\.db//;
+  $query          .= "$table ";
+}
+
+# Use this to get the column header names, for 
+# later checking.
+$sth      = $dbh->prepare( "SELECT * from $table;" );
+$return   = $sth->execute() or die "$DBI::errstr";
+if ( $return < 0 ) {
+  print $DBI::errstr;
+}
+my $fields_ref  = $sth->{NAME};
+my @fields      = values @$fields_ref;
+my %fields      = map { $_ => 1 } @fields;
+
+if ( $column && $like ) {
+  $fields{$column} or die "There is no $column column";
+  $query  .= "WHERE $column LIKE '$like' ";
+} 
+
+# Finalize the query.
+$query  .= ";";
+
+sub show_character {
+  my ( $year, $cadre, $id, $last_name, $first_name, $gender, 
+    $upp, $birth_year, $birth_day, $notes) = @_;
+  my $line  = "$first_name $last_name [$upp] $gender\n";
+  $line     .= $notes;
+  $line     .= "\n"; 
+  print $line;
+}
+
+$sth      = $dbh->prepare( $query );
+$return   = $sth->execute() or die "$DBI::errstr";
 if ( $return < 0 ) {
   print $DBI::errstr;
 }
 
 while ( my @row = $sth->fetchrow_array()) {
-  print "$row[4] $row[3] has a birthday on $row[8].\n";
+  print "---\n";
+  show_character(@row);
+  print "\n";
 }
 
 $dbh->disconnect();
-
 
