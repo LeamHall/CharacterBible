4a679046d247808e04ba4b8f21b89e1bf7f49e3f
Leam Hall <leamhall@gmail.com>
2021-09-18 17:56:47 -0500

    Trying out a setUp setup for tests.

--- test/test_character_builder.py
+++ test/test_character_builder.py
@@ -8,42 +8,31 @@
 
 import unittest
 
-#import sys
-#sys.path.append('person')
 from person import Person
 from person import Character
 from person import CharacterBuilder
 
-#stats     = {'soc': 13}
-#data      = {'first_name': 'Amanda', 'last_name': 'Lefron', 'gender': 'F', 'stats' : stats}
-#builder   = CharacterBuilder(data)
-#character = builder.return_character()
-#print(character.supp_4())
 class TestCharacterBuilder(unittest.TestCase):
-
-  def testCreation(self):
-    builder = CharacterBuilder()
-    character = builder.return_character()
-    self.assertIsInstance(builder, CharacterBuilder)
-    self.assertIsInstance(character, Character)
-    self.assertIsInstance(character, Person)
-
-  def testCreateName(self):
-    data = {'first_name' : 'Amanda', 'last_name': 'Lefron'}
-    builder = CharacterBuilder(data)
-    character = builder.return_character()
-    self.assertEqual(character.first_name, 'Amanda')
-    self.assertEqual(character.last_name, 'Lefron')
-
-  def testCreateStats(self):
+  def setUp(self):
     stats     = {'soc': 12}
     data      = {'first_name': 'Amanda', 'last_name': 'Lefron', 'gender': 'F', 'stats' : stats}
-    builder = CharacterBuilder(data)
-    character = builder.return_character()
-    self.assertEqual(character.stats['soc'], 12)
-    for s in character.stats.keys():
-      self.assertGreaterEqual(character.stats[s], 2)
-      self.assertLessEqual(character.stats[s], 12)
+    self.builder = CharacterBuilder(data)
+    self.character = self.builder.return_character()
+  
+  def testCreation(self):
+    self.assertIsInstance(self.builder, CharacterBuilder)
+    self.assertIsInstance(self.character, Character)
+    self.assertIsInstance(self.character, Person)
+
+  def testCreateName(self):
+    self.assertEqual(self.character.first_name, 'Amanda')
+    self.assertEqual(self.character.last_name, 'Lefron')
+
+  def testCreateStats(self):
+    self.assertEqual(self.character.stats['soc'], 12)
+    for s in self.character.stats.keys():
+      self.assertGreaterEqual(self.character.stats[s], 2)
+      self.assertLessEqual(self.character.stats[s], 12)
 
 
 
