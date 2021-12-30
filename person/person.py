23706afb1b7535f6d4252056cbdcc4adf154c85f
Leam Hall <leamhall@gmail.com>
2021-12-30 08:45:24 -0600

    Brought person dataclass in line with database.

--- person/person.py
+++ person/person.py
@@ -7,41 +7,26 @@
 # desc:     Base Person object.
 
 from dataclasses import dataclass, field
-# from typing import Any
 
 @dataclass
 class Person:
   """ Base Person object """
 
-  first_name:     str   = ''
-  last_name:      str   = ''
-  gender:         str   = ''
-  birth_info:     dict  = field(default_factory=dict)
-  notes:          str   = ''
-  idx:            int   = -1
+  idx:            int = -1
+  last_name:      str = ''
+  first_name:     str = ''
+  middle_name:    str = ''
+  gender:         str = ''
+  birthdate:      int = ''
+  plot:           str = ''
+  temperament:    str = ''
+  notes:          str = ''
 
   def set_attr(self, attr, value, key = None):
-    # For the list and the dict, it assumes they already exist.
-    try:
-      attr_type = getattr(self, attr)
-      if isinstance(attr_type, list):
-        attr_type.append(value)
-      elif isinstance(attr_type, dict) and key != None:
-        if key:
-          print("in dict, key is " + key)
-        attr_type[key] = value
-      else:
-        attr_type = value
-    except:
-      setattr(self, attr, value)
+    setattr(self, attr, value)
  
   def get_attr(self, attr):
     if hasattr(self, attr):
       return getattr(self,attr)
     else:
       return None
-
-if __name__ == "__main__":
-  import doctest
-  doctest.testmod()
- 
--- test/test_person.py
+++ test/test_person.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 # name:     test_person.py
-# version:  0.0.2
-# date:     20211225
+# version:  0.0.3
+# date:     20211230
 # author:   Leam Hall
 # desc:     Testing Base Person object.
 
@@ -21,19 +21,8 @@ def test_set_attr_string(person):
   expected_attr = 'Ester'
   assert person.get_attr('middle_name') == expected_attr
 
-@pytest.mark.skip("needs a different attribute")
-def test_set_attr_list(person):
-  pass
-
-@pytest.mark.skip("needs a different attribute")
-def test_set_attr_dict(person):
-  k = 'plot'
-  person.set_attr('mental', 'fight great evil', k)
-  expected_attr = 'fight great evil'
-  mental = getattr(person, 'mental')
-  assert mental[k] == expected_attr
-
 def test_get_attr_fail(person):
   fail = person.get_attr('kissing habits')
   assert fail == None
+
  
