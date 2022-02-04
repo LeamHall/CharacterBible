c5c84e6a2bdffc78b26387ae9b0def5cf383e3ab
Leam Hall <leamhall@gmail.com>
2022-02-04 10:28:14 -0600

    Updated document for volume and ip address.

---
+++ docker/app/person/character_builder.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# name:     character_builder.py
+# version:  0.0.1
+# date:     20210612
+# author:   Leam Hall
+# desc:     Builder for Character Object
+
+import random
+from person import Character
+from person import Person
+#from character import Character
+
+#import person
+
+#  birth_year: int = 0
+#  birth_day:  int = 0
+#  notes:      str = ''
+
+class CharacterBuilder:
+ 
+  def __init__(self, data = {}):
+    self.character = Character()
+    self.set_stats(data)
+    self.set_data(data)
+    self.character.gen_upp()
+    self.return_character()
+    
+
+  def set_stats(self, data):
+    stats = data.get('stats', {})
+    stat_data = {}
+    for stat in ['str', 'dex', 'end', 'int', 'edu', 'soc']:
+      stat_data[stat] = stats.get(stat, self.roll_2d6())
+    self.character.set_stats(stat_data)
+      
+
+  def roll_2d6(self):
+    return random.randint(1,6) + random.randint(1,6)
+ 
+  def set_data(self, data):
+    self.character.gender     = data.get('gender', self.gen_gender())
+    self.character.first_name = data.get('first_name', self.gen_first_name(self.character.gender))
+    self.character.last_name  = data.get('last_name', self.gen_last_name())
+
+  def gen_gender(self):
+    return 'M'
+
+  def gen_first_name(self, gender):
+    return 'Fred'
+
+  def gen_last_name(self):
+    return 'Smith'
+
+  def return_character(self):
+    return self.character
+  
+  
---
+++ docker/main.py
@@ -0,0 +1,46 @@
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import subprocess
+
+app = FastAPI()
+
+body="<html>" \
+  "<body><h2>Look for people</h2>"\
+  "<a href='/people'>Get everyone</a>"\
+  "</body></html>"
+
+form    = """
+  <form action="http://0.0.0.0:8000/people" method="GET">
+  <input type="submit">
+  </form>
+  """
+
+rundir  = '/app/'
+datadir = '/data/'
+cmd_str = f"{rundir}character_bible.py"
+options = ['-D', datadir, '-o', 'html']
+cmd     = [cmd_str]
+cmd     = cmd + options
+
+@app.get("/people/{idx}", response_class=HTMLResponse)
+async def get_people(idx: str):
+  idx_cmd       = cmd + ['-I', idx]
+  query_result  = subprocess.run(idx_cmd, capture_output = True)
+  query_data    = query_result.stdout.decode("utf-8")
+  query_data    = query_data.replace("\n", "")
+  html_response = f"<html><body></p>{query_data}</body></html>"
+  return HTMLResponse(content=html_response, status_code=200)
+
+@app.get("/people", response_class=HTMLResponse)
+async def get_people():
+  query_result  = subprocess.run(cmd, capture_output = True)
+  query_data    = query_result.stdout.decode("utf-8")
+  query_data    = query_data.replace("\n", "")
+  html_response = f"<html><body></p>{query_data}</body></html>"
+  return HTMLResponse(content=html_response, status_code=200)
+
+@app.get("/")
+async def index():
+  return HTMLResponse(content=body)
+
