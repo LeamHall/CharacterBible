3e0159743146c4b273095e54a75538efc6b99143
Leam Hall <leamhall@gmail.com>
2023-12-04 07:22:12 -0600

    Fixed the psycopg in requirements.txt.

--- docker/django/code/django_project/settings.py
+++ docker/django/code/django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "POST": 5432,
     }
 }
 
