6321218a04ed63921b4967be9c0a69fd8ac23f97
Leam Hall <leamhall@gmail.com>
2023-12-23 10:03:57 -0600

    Got login page working.

--- docker/django/code/django_project/urls.py
+++ docker/django/code/django_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("characters/", include("characters.urls")),
     path("", include("pages.urls")),
 ]
